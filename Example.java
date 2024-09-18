5ae706b3dde049d44d919e00303381e6a81111bf
Lakkappa Basidoni <lakkappabasidoni2000@gmail.com>
2024-09-18 12:50:33 +0530

    combined commit

--- Example.java
+++ Example.java
@@ -2,8 +2,10 @@ public class Example{
 
     public static void main(String[] args){
         System.out.println("Hello world!");
-
         System.out.println("Hi....");
+        System.out.println("first line");
+        System.out.println("second line");
+        System.out.println("third line");
 
         for(int i=0; i<5;i++){
             System.out.println("  >>  "+i);
